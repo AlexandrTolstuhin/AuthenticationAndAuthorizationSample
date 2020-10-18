de6735d13be491ce87da58f96cd621db823d8770
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-18 15:47:03 +0500

    Add login form

--- AuthenticationAndAuthorizationSample.Basics/Controllers/AdminController.cs
+++ AuthenticationAndAuthorizationSample.Basics/Controllers/AdminController.cs
@@ -1,11 +1,30 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AuthenticationAndAuthorizationSample.Basics.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationAndAuthorizationSample.Basics.Controllers
 {
+    [Authorize]
     public class AdminController : Controller
     {
         public IActionResult Index()
         {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel { ReturnUrl = returnUrl });
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
             return View();
         }
     }
