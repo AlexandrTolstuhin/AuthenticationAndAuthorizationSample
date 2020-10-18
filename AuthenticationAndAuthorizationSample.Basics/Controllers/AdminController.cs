d8ff811b88bc03bbc08aa37b7c73f1425b319702
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-18 15:56:23 +0500

    Add sign in Cookie

--- AuthenticationAndAuthorizationSample.Basics/Controllers/AdminController.cs
+++ AuthenticationAndAuthorizationSample.Basics/Controllers/AdminController.cs
@@ -1,4 +1,8 @@
-﻿using AuthenticationAndAuthorizationSample.Basics.ViewModels;
+﻿using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using AuthenticationAndAuthorizationSample.Basics.ViewModels;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -20,12 +24,21 @@ namespace AuthenticationAndAuthorizationSample.Basics.Controllers
 
         [HttpPost]
         [AllowAnonymous]
-        public IActionResult Login(LoginViewModel model)
+        public async Task<IActionResult> Login(LoginViewModel model)
         {
             if (!ModelState.IsValid)
                 return View(model);
 
-            return View();
+            var claims = new List<Claim>
+            {
+                new Claim("Demo", "Value")
+            };
+            var identity = new ClaimsIdentity(claims, "Cookie");
+            var principal = new ClaimsPrincipal(identity);
+
+            await HttpContext.SignInAsync("Cookie", principal);
+
+            return Redirect(model.ReturnUrl);
         }
     }
 }
