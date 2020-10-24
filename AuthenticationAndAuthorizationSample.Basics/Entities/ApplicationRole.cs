4b893f4ff6518ece02a1b5cf875a8a94cb2bf4ab
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-24 12:35:57 +0500

    Add ApplicationDbContext

---
+++ AuthenticationAndAuthorizationSample.Basics/Entities/ApplicationRole.cs
@@ -0,0 +1,8 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthenticationAndAuthorizationSample.Basics.Entities
+{
+    public class ApplicationRole : IdentityRole<Guid>
+    { }
+}
