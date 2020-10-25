95ed03c9b3e859f4f39b55faa9aaf889fc9e8761
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-25 12:53:49 +0500

    Add AccountController and move authorization logic to it

--- AuthenticationAndAuthorizationSample.Basics/Controllers/SecureController.cs
+++ AuthenticationAndAuthorizationSample.Basics/Controllers/SecureController.cs
@@ -1,11 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Security.Claims;
-using System.Threading.Tasks;
-using AuthenticationAndAuthorizationSample.Basics.Entities;
-using AuthenticationAndAuthorizationSample.Basics.ViewModels;
-using Microsoft.AspNetCore.Authentication;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Identity;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AuthenticationAndAuthorizationSample.Basics.Controllers
@@ -13,17 +6,6 @@ namespace AuthenticationAndAuthorizationSample.Basics.Controllers
     [Authorize]
     public class SecureController : Controller
     {
-        private readonly UserManager<ApplicationUser> _userManager;
-        private readonly SignInManager<ApplicationUser> _signInManager;
-
-        public SecureController(
-            UserManager<ApplicationUser> userManager, 
-            SignInManager<ApplicationUser> signInManager)
-        {
-            _userManager = userManager;
-            _signInManager = signInManager;
-        }
-
         public IActionResult Index()
         {
             return View();
@@ -39,43 +21,6 @@ namespace AuthenticationAndAuthorizationSample.Basics.Controllers
         public IActionResult Administrator()
         {
             return View();
-        }
-
-        [AllowAnonymous]
-        public IActionResult Login(string returnUrl)
-        {
-            return View(new LoginViewModel { ReturnUrl = returnUrl });
-        }
-
-        [HttpPost]
-        [AllowAnonymous]
-        public async Task<IActionResult> Login(LoginViewModel model)
-        {
-            if (!ModelState.IsValid)
-                return View(model);
-
-            var user = await _userManager.FindByNameAsync(model.UserName);
-
-            if (user == null)
-            {
-                ModelState.AddModelError("", "User not found");
-                return View(model);
-            }
-
-            var result = await _signInManager.PasswordSignInAsync(user, model.Password, false, false);
-
-            if (result.Succeeded) 
-                return Redirect(model.ReturnUrl);
-
-            ModelState.AddModelError("", "Incorrect password");
-            return View(model);
-        }
-
-        public async Task<IActionResult> Logout()
-        {
-            await _signInManager.SignOutAsync();
-
-            return RedirectToAction("Index", "Home");
         }
     }
 }
