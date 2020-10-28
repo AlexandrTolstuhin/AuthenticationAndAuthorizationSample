a2e4f0931c897ea29cdf96561729893ca89baf0c
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-28 13:07:45 +0500

    Configure JwtBearer

--- AuthenticationAndAuthorizationSample.JwtBearer/Startup.cs
+++ AuthenticationAndAuthorizationSample.JwtBearer/Startup.cs
@@ -1,7 +1,11 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AuthenticationAndAuthorizationSample.JwtBearer
 {
@@ -9,6 +13,33 @@ namespace AuthenticationAndAuthorizationSample.JwtBearer
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddAuthentication("OAuth")
+                .AddJwtBearer("OAuth", options =>
+                {
+                    var secretBytes = Encoding.UTF8.GetBytes(Constants.SecretKey);
+
+                    var key = new SymmetricSecurityKey(secretBytes);
+
+                    options.Events = new JwtBearerEvents
+                    {
+                        OnMessageReceived = context =>
+                        {
+                            if (context.Request.Query.ContainsKey("t"))
+                            {
+                                context.Token = context.Request.Query["t"];
+                            }
+                            return Task.CompletedTask;
+                        }
+                    };
+
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidIssuer = Constants.Issuer,
+                        ValidAudience = Constants.Audience,
+                        IssuerSigningKey = key
+                    };
+                });
+
             services.AddControllersWithViews();
         }
 
@@ -20,6 +51,9 @@ namespace AuthenticationAndAuthorizationSample.JwtBearer
             }
 
             app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
