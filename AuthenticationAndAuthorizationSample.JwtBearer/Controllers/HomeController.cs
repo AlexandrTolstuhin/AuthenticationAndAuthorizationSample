7e39d354c10502f860ea242c8c4d99b7e22fb417
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-28 13:07:22 +0500

    Add jwt token generation 2

--- AuthenticationAndAuthorizationSample.JwtBearer/Controllers/HomeController.cs
+++ AuthenticationAndAuthorizationSample.JwtBearer/Controllers/HomeController.cs
@@ -1,4 +1,12 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using JwtRegisteredClaimNames = Microsoft.IdentityModel.JsonWebTokens.JwtRegisteredClaimNames;
 
 namespace AuthenticationAndAuthorizationSample.JwtBearer.Controllers
 {
@@ -9,8 +17,38 @@ namespace AuthenticationAndAuthorizationSample.JwtBearer.Controllers
             return View();
         }
 
+        [Authorize]
         public IActionResult Secret()
         {
+            return View();
+        }
+
+        public IActionResult Authenticate()
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Sub, "Subject"),
+                new Claim(JwtRegisteredClaimNames.Email, "subject@example.com")
+            };
+
+            var secretBytes = Encoding.UTF8.GetBytes(Constants.SecretKey);
+
+            var key = new SymmetricSecurityKey(secretBytes);
+
+            var signingCredentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var token = new JwtSecurityToken(
+                Constants.Issuer, 
+                Constants.Audience, 
+                claims, 
+                DateTime.Now, 
+                DateTime.Now.AddHours(1), 
+                signingCredentials);
+
+            var value = new JwtSecurityTokenHandler().WriteToken(token);
+
+            ViewBag.Token = value;
+
             return View();
         }
     }
