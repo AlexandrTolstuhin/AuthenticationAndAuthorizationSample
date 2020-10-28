781b4dad1a2f298f029875b197549700cf9de327
Alexandr Tolstuchin <alex.tolstuchin@gmail.com>
2020-10-28 13:06:07 +0500

    Add jwt token generation

---
+++ AuthenticationAndAuthorizationSample.JwtBearer/Constants.cs
@@ -0,0 +1,9 @@
+﻿namespace AuthenticationAndAuthorizationSample.JwtBearer
+{
+    public class Constants
+    {
+        public const string Issuer = "https://localhost:5001";
+        public const string Audience = "https://localhost:5001";
+        public const string SecretKey = "This_is_my_very_long_secret_key_for_jwt_token_generation";
+    }
+}
